b481780e3429c2207fbf8db6ff85ccd95d13d1e6
straris <straris@hotmail.com>
2016-01-17 18:52:28 +0100

    Add page object for Search Results with currency parsing

---
+++ src/main/java/selenium/pageObjects/SearchResultsPage.java
@@ -0,0 +1,35 @@
+package selenium.pageObjects;
+
+/**
+ * Created by stratos on 17/01/2016.
+ */
+
+import org.openqa.selenium.WebElement;
+import selenium.modules.WebDriverFactory;
+import java.io.IOException;
+import java.util.List;
+
+public class SearchResultsPage {
+    WebDriverFactory webfactory = new WebDriverFactory();
+
+    public SearchResultsPage SortByPrice() throws IOException {
+        webfactory.findMyElement("buttonSortByPrice").click();
+        List<WebElement> beforeCommaList = webfactory.findAllElements("labelCurrencyBeforecomma");
+        List<WebElement> decimalsList = webfactory.findAllElements("labelCurrencyDecimals");
+        List<Float> currencyList = null;
+        float[] currencies;
+        currencies = new float[beforeCommaList.size()];
+        for (int i = 0; i < beforeCommaList.size(); i++) {
+            System.out.println(decimalsList.get(i).getText());
+            currencies[i]=formatCurrency(beforeCommaList.get(i).getText(),decimalsList.get(i).getText());
+        }
+        return this;
+    }
+
+    public Float formatCurrency(String beforeComma, String Decimals ){
+        int firstPart = Integer.parseInt(beforeComma);
+        float decimals = Float.parseFloat(Decimals.replaceAll("\\D+",""))/100;
+        float finalCurrency = firstPart+decimals;
+        return finalCurrency;
+    }
+}
